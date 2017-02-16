6b59abe68684540068f4adf70524a783a850b481
HuDP <mox113@foxmail.com>
2017-02-17 07:23:45 +0800

    Update Retrofit

--- app/src/main/java/cn/hudp/androiddevartnote/Retrofit/API.java
+++ app/src/main/java/cn/hudp/androiddevartnote/Retrofit/API.java
@@ -1,9 +1,14 @@
 package cn.hudp.androiddevartnote.Retrofit;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by HuDP on 2017/2/9.
  */
 
 public interface API {
-
+    @GET("top250")
+    Call<DoubanMovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
 }
--- app/src/main/java/cn/hudp/androiddevartnote/Retrofit/RetrofitActivity.java
+++ app/src/main/java/cn/hudp/androiddevartnote/Retrofit/RetrofitActivity.java
@@ -2,8 +2,12 @@ package cn.hudp.androiddevartnote.Retrofit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import cn.hudp.androiddevartnote.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,15 +17,26 @@ public class RetrofitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
-
+        post();
     }
 
     public void post() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("")
+                .baseUrl("https://api.douban.com/v2/movie/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        retrofit.create(API.class);
+        API api = retrofit.create(API.class);
+        Call<DoubanMovieEntity> call = api.getTopMovie(1, 10);
+        call.enqueue(new Callback<DoubanMovieEntity>() {
+            @Override
+            public void onResponse(Call<DoubanMovieEntity> call, Response<DoubanMovieEntity> response) {
+                Log.e("POST", response.toString());
+            }
 
+            @Override
+            public void onFailure(Call<DoubanMovieEntity> call, Throwable t) {
+                Log.e("POST", t.toString());
+            }
+        });
     }
 }
