656c20b9b4ce599012a3c72e8ceace0a7c80077f
HuDP <mox113@foxmail.com>
2017-06-07 18:31:04 +0800

    Drawable 继续学习

--- app/src/main/java/cn/hudp/androiddevartnote/Drawable_6/DrawableActivity.java
+++ app/src/main/java/cn/hudp/androiddevartnote/Drawable_6/DrawableActivity.java
@@ -1,6 +1,5 @@
 package cn.hudp.androiddevartnote.Drawable_6;
 
-import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -16,16 +15,14 @@ public class DrawableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drawable);
 
         ivLevei = (ImageView) findViewById(R.id.iv_levei);
-//        Drawable drawable = getDrawable(R.drawable.levei_list_drawable);
-//        drawable.setLevel(1);
-//        ivLevei.setBackground(drawable);
+        ivLevei.getDrawable().setLevel(2);
 
         //TransitionDrawable
 //        TransitionDrawable drawable = (TransitionDrawable) ivLevei.getBackground();
 //        drawable.startTransition(1000);
 
-        ScaleDrawable scaleDrawable = (ScaleDrawable) ivLevei.getBackground();
-        scaleDrawable.setLevel(1);
+//        ScaleDrawable scaleDrawable = (ScaleDrawable) ivLevei.getBackground();
+//        scaleDrawable.setLevel(1);
 
 //        ClipDrawable clipDrawable = (ClipDrawable) ivLevei.getBackground();
 //        clipDrawable.setLevel(7000);
